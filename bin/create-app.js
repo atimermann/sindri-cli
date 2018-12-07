e6b93add7861ec0c14be1988c7263bd60d3e7921
André Timermann <andre@timermann.com.br>
2018-12-07 13:54:04 -0200

    Criação script create-app

--- bin/create-app.js
+++
@@ -1,9 +0,0 @@
-/**
- * **Created on 07/12/18**
- *
- * <File Reference Aqui: create-app>
- * @author André Timermann <andre.timermann@smarti.io>
- *
- */
-'use strict'
-
---
+++ template/app/controllers/__controller_template.js
@@ -0,0 +1,43 @@
+/**
+ * **Created on {{CREATED_DATE}}**
+ *
+ * apps/{{APP}}/controllers/{{CONTROLLER_FILE_NAME}}
+ * @author {{AUTHOR}}
+ *
+ */
+'use strict'
+
+
+const Controller = require('sindri-framework/controller')
+const logger = require('sindri-framework/logger')
+const config = require('sindri-framework/config')
+
+class {{CONTROLLER_NAME}}Controller extends Controller {
+
+  /**
+   * Inicialização
+   */
+  setup() {
+
+    logger.info('ok')
+
+  }
+
+
+  /**
+   * Configuração de Rotas
+   */
+  routes() {
+
+    this.get('/', async (request, response) => {
+
+      response
+        .status(200)
+        .send('OK')
+
+    })
+
+  }
+}
+
+module.exports = {{CONTROLLER_NAME}}Controller
