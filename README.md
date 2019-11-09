2f006c720f5f0e81d06f510057b6eac5605981e2
André Timermann <andre@timermann.com.br>
2019-11-09 03:42:06 -0300

    - Atualizado pkg 4.4.0 e node 12 (agora corretamente) - sindri-cli build agora utiliza npm run build - Correção no install-assets - Removido obrigatoriedade de utilizar git - Permite criar projeto com diretório não vazio, agora só emite alerta

--- README.md
+++ README.md
@@ -47,4 +47,4 @@ Ao atualizar o projeto verificar se foi atualizado a versão do:
 ## Versões Testadas
 
 * Sindri Cli@2.0.1 => Sindri Framework@2.0
-* Sindri Cli@2.1.0 => Sindri Framework@2.1 => pkg@4.3.4  
+* Sindri Cli@2.1.0 => Sindri Framework@2.1 => pkg@4.4.0  
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,14 +3,17 @@
 
 echo "Instalando dependencias..."
 
-if [[ -x "$(command -v git)" ]]; then
-    git init
-fi
+
+# Não obrigar usar git
+#if [[ -x "$(command -v git)" ]]; then
+#    git init
+#fi
 
 if ! [[ -x "$(command -v npx)" ]]; then
     npm i -g --loglevel warn npx
 fi
 
 npm i --save --loglevel warn sindri-framework
-npm i --save --loglevel warn pkg@4.3.4
-npm i --save --loglevel warn config
+npm i --save --loglevel warn pkg@4.4.0
+# Foi incorporado ao projeto  TODO: remover depois de testado
+# npm i --save --loglevel warn config
