a9cbd5e738047b0ffda451b9e6ee64194624b4c1
André Timermann <andre@timermann.com.br>
2023-01-29 16:24:17 -0300

    melhoria controller de teste

--- src/template/app/controllers/__controller_template.mjs
+++ src/template/app/controllers/__controller_template.mjs
@@ -13,7 +13,7 @@ export default class {{CONTROLLER_NAME}}Controller extends Controller {
    */
 
   setup () {
-    logger.info(`App "{{APP}}" Controller "{{CONTROLLER_FILE_NAME}}" OK. Static route: ${config.get('server.staticRoute')}`)
+    logger.info(`App "{{APP}}" Controller "{{CONTROLLER_FILE_NAME}}" OK.`)
   }
 
   /**
@@ -23,7 +23,7 @@ export default class {{CONTROLLER_NAME}}Controller extends Controller {
     this.get('/{{CONTROLLER_ROUTE_NAME}}', async (request, response) => {
       response
         .status(200)
-        .send('OK')
+        .send(`App "inventory" Controller "product-category.mjs" OK. Port: ${config.get('server.port')}`)
     })
   }
 }
